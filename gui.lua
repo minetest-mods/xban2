877e55078718047323ce2d5b07d004c05414aaf5
Agaran <agaran@pld-linux.org>
2016-11-26 20:28:17 +0100

    Filter fields submission by privs.

--- gui.lua
+++ gui.lua
@@ -100,6 +100,11 @@ end
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname ~= FORMNAME then return end
 	local name = player:get_player_name()
+	if not minetest.check_player_privs(name, { ban=true }) then
+		minetest.log("warning",
+				"[xban2] Received fields from unauthorized user: "..name)
+		return
+	end
 	local state = get_state(name)
 	if fields.player then
 		local t = minetest.explode_textlist_event(fields.player)
