3b700453651e7119c2c48c62d009ed2edf336c93
Diego Martínez <kaeza@users.noreply.github.com>
2019-02-12 16:36:06 -0300

    Update to 5.0 auth.

--- gui.lua
+++ gui.lua
@@ -9,7 +9,7 @@ local ESC = minetest.formspec_escape
 local function make_list(filter)
 	filter = filter or ""
 	local list, n, dropped = { }, 0, false
-	for k in pairs(minetest.auth_table) do
+	for k in minetest.get_auth_handler().iterate() do
 		if strfind(k, filter, 1, true) then
 			if n >= MAXLISTSIZE then
 				dropped = true
